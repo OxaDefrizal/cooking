0ff912fba7cbfff0571d05adfd048d62808c553d
OxaDefrizal <oxadefrizal@students.amikom.ac.id>
2024-05-30 23:37:38 +0700

    add Navigation Bar

--- src/component/NavigationBar.jsx
+++ src/component/NavigationBar.jsx
@@ -1,29 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/cooking-canvas-logo-black.svg";
 
 export default function NavigationBar() {
   return (
-    <nav className="bg-blue-600 p-4">
-      <ul className="flex space-x-4 text-white">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/recipes">Recipes</Link>
-        </li>
-        <li>
-          <Link to="/about">About Us</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact Us</Link>
-        </li>
-        <li>
-          <Link to="/signin">Sign In</Link>
-        </li>
-        <li>
-          <Link to="/signup">Sign Up</Link>
-        </li>
-      </ul>
+    // container navigation bar
+    <nav className="flex h-40 w-full items-center justify-between bg-gray-50 px-20 py-8 shadow-md">
+      {/* Cooking Canvas Logo */}
+      <img
+        src={logo}
+        alt="Cooking Canvas Logo"
+        className="h-auto max-h-24 w-auto max-w-24"
+      />
+      {/* Container Button Navigation Bar */}
+      <div className="flex h-auto w-auto items-center justify-center gap-10">
+        {/* Button Home */}
+        <Link to={"/"} className="h-auto w-auto">
+          <button className="rounded-md bg-gray-900 px-4 py-2 text-white">
+            Home
+          </button>
+        </Link>
+        {/* Button Menu Recipe */}
+        <Link to={"/recipes"} className="h-auto w-auto">
+          <button className="rounded-md bg-gray-900 px-4 py-2 text-white">
+            Recipe
+          </button>
+        </Link>
+        {/* Button About Us */}
+        <Link to={"/about"} className="h-auto w-auto">
+          <button className="rounded-md bg-gray-900 px-4 py-2 text-white">
+            About Us
+          </button>
+        </Link>
+        {/* Button Contact Us */}
+        <Link to={"/contact"} className="h-auto w-auto">
+          <button className="rounded-md bg-gray-900 px-4 py-2 text-white">
+            Contact
+          </button>
+        </Link>
+        {/* Button Sign In */}
+        <Link to={"/signin"} className="h-auto w-auto">
+          <button className="rounded-md bg-gray-900 px-4 py-2 text-white">
+            Sign In
+          </button>
+        </Link>
+        {/* Button Sign Up */}
+        <Link to={"/signup"} className="h-auto w-auto">
+          <button className="rounded-md bg-gray-900 px-4 py-2 text-white">
+            Sign Up
+          </button>
+        </Link>
+      </div>
     </nav>
   );
 }
